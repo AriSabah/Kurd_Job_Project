6950d402573980c3b3bb6b64f5178058ede9fc2e
AriSabah <77207184+AriSabah@users.noreply.github.com>
2022-08-27 17:10:01 +0300

    V1.0.2

--- components/Home/Features.jsx
+++ components/Home/Features.jsx
@@ -6,7 +6,13 @@ const Features = () => {
   return (
     <div className="lg:p-10 md:p-5">
       <h1 className="text-5xl text-accent text-center pb-4 sm:pt-4 md-pt-0">Kurd Job Features</h1>
-      <Card icon='icons/ads_click.svg' title='Easy To Use' description='' />
+
+      <Card
+        icon="icons/ads_click.svg"
+        title="Easy To Use"
+        description="vgaegv avrfgv dvsf dc sdva dsv dsv dsfsef ds esv vgaegv avrfgv dvsf dc sdva dsv dsv dsfsef ds esv ghhffg ggf kjhb"
+      />
+      
     </div>
   );
 };
--- components/UI/Card.jsx
+++ components/UI/Card.jsx
@@ -1,14 +1,34 @@
 import React from "react";
+import { motion } from "framer-motion";
+
+import style from './Card.module.css';
+
+const cardAnimation = {
+  initial: {
+    'box-shadow': 'inset 0 0 1em #2666CF88',
+    background: 'rgb(1,50,75)',
+    background: 'linear-gradient(0deg, rgba(1,50,75,1) 0%, rgba(1,50,75,1) 92%, rgba(15,91,130,0.9) 100%'
+  },
+  animate: {
+    background: 'linear-gradient(360deg, rgba(1,50,75,1) 0%, rgba(1,50,75,1) 92%, rgba(15,91,130,0.9) 100%',
+    transition: {
+      duration: 8,
+      repeat: Infinity,
+      repeatType: "loop",
+      repeatDelay: 0
+    },
+  },
+}
 
 const Card = ({title, description, icon}) => {
   return (
-    <div className="card w-96 bg-neutral shadow-xl">
+    <motion.div className="card w-96 bg-neutral bg-opacity-50" initial='initial' animate='animate' variants={cardAnimation}>
       <div className="card-body">
-        <img className='w-24 mx-auto mb-3' src={icon} alt={title} />
+        <img className={`w-24 mx-auto mb-3 ${style.icon}`} src={icon} alt={title} />
         <h2 className="text-3xl text-center text-accent font-bold">{title}</h2>
         <p className="text-center">{description}</p>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
